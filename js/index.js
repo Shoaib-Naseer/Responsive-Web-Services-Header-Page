b8aa55277ff3fd7dcba6c7e0fb44aee8908b11a5
Shoaib Naseer <43854873+Shoaib-Naseer@users.noreply.github.com>
2023-03-05 18:41:58 +0500

    Completed all sections

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
 $(document).ready(function () {
-  if ($(window).scrollTop() > 30) {
-    $('nav').css({
-      'background-color': '#6C5CE7',
-      'box-shadow': '0 0.2rem 0.5rem rgba(0,0,0,.4)',
-    });
-  } else {
-    $('nav').css({
-      'background-color': 'none',
-      'box-shadow': 'none',
-    });
-  }
+ 
 
   $('.fa-bars').click(function () {
     $(this).toggleClass('fa-times');
@@ -19,5 +9,20 @@ $(document).ready(function () {
   $(window).on('load scroll', function () {
     $('.fa-bars').removeClass('fa-times');
     $('.menu').removeClass('menu-toggle');
+    if ($(window).scrollTop() > 30) {
+      $('.header').css({
+        background: '#6C5CE7',
+        'box-shadow': '0 .2rem .5rem rgba(0,0,0,.4)',
+      });
+    } else {
+      $('.header').css({ background: 'none', 'box-shadow': 'none' });
+    }
+  });
+
+  $('.accordion-header').click(function () {
+    $('.accordion .accordion-body').slideUp();
+    $(this).next('.accordion-body').slideDown();
+    $('.accordion .accordion-header span').text('+');
+    $(this).children('span').text('-');
   });
 });
